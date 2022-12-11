22edf242c57675b2c7088a5d1dd5f739225ffef7
Alex Braha Stoll <alexbrahastoll@gmail.com>
2022-12-11 20:53:13 -0300

    Adds ideas for the changes to be made for version 2 of our working days calculator

---
+++ v2/working_day_calculator.rb
@@ -0,0 +1,3 @@
+# calculate_work_days
+# - Should be able to receive an interval as the period under consideration
+# - Should be able to receive as days off an array containing both individual dates as well as periods 
