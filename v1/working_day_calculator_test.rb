c14f2c6d4acbd3f404360fffac59069c1d93b6a8
Alex Braha Stoll <alexbrahastoll@gmail.com>
2022-12-30 19:59:30 -0300

    V1 impl with RBS::UnknownTypeName error

--- v1/working_day_calculator_test.rb
+++ v1/working_day_calculator_test.rb
@@ -3,6 +3,8 @@ require_relative './working_day_calculator'
 calc = WorkingDayCalculator.new
 calc.period_start = Date.new(2023, 1, 1)
 calc.period_end = Date.new(2023, 1, 31)
-calc.add_day_off(Date.new(2023, 1, 1))
+
+# calc.add_day_off(Date.new(2023, 1, 1)) # original, correct usage of #add_day_off
+calc.add_day_off('BOOM') # incorrectly passing a String to #add_day_off
 
 puts calc.calculate_working_days # Expected: 30
