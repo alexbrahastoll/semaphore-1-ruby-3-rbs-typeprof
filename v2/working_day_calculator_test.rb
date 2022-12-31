0ddf5ee3c95898df20499ef6433b69857b2c62d1
Alex Braha Stoll <alexbrahastoll@gmail.com>
2022-12-31 00:03:32 -0300

    V2 impl with three Ruby::ArgumentTypeMismatch errors

--- v2/working_day_calculator_test.rb
+++ v2/working_day_calculator_test.rb
@@ -5,4 +5,17 @@ period_start = Date.new(2023, 1, 1)
 period_end = Date.new(2023, 1, 31)
 days_off = [Date.new(2023, 1, 1), Date.new(2023, 1, 29)..Date.new(2023, 1, 30)]
 
-puts calc.calculate_working_days(period_start..period_end, days_off) # Expected: 28
+# correct! prints "28"
+puts calc.calculate_working_days(period_start..period_end, days_off)
+
+# type error, arg 1 must be a range of dates
+calc.calculate_working_days([], days_off)
+
+# type error, arg 2 must be an array composed of dates and / or ranges of dates
+calc.calculate_working_days(period_start..period_end, Date.new(2023, 1, 1))
+
+# type error, arg 2 must be an array composed of only dates and / or ranges of dates
+calc.calculate_working_days(period_start..period_end, [
+  Date.new(2023, 1, 1),
+  '2023-01-15',
+  Date.new(2023, 1, 29)..Date.new(2023, 1, 30)])
